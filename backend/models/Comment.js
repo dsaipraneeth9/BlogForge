c77e6f0d8643b4bb26582e7c215696fd1da8f357
dsaipraneeth9 <dsp.saipraneeth@gmail.com>
2025-02-13 09:28:19 +0530

    BlogForge Backend

---
+++ backend/models/Comment.js
@@ -0,0 +1,18 @@
+import { Schema,model } from "mongoose";
+const commentSchema=new Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    blog:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    }
+})
+export default model("Comment",commentSchema);
